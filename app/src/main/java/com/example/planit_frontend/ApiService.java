613bb4bffd9bbf5f118bc3adfca56221adee46b0
Trinity Booth <trinitybooth89@gmail.com>
2025-02-27 20:54:33 +0000

    Initial commit

---
+++ app/src/main/java/com/example/planit_frontend/ApiService.java
@@ -0,0 +1,13 @@
+package com.example.planit_frontend;
+import retrofit2.Call;
+import retrofit2.http.Body;
+import retrofit2.http.GET;
+import retrofit2.http.POST;
+import retrofit2.http.Path;
+public interface ApiService {
+/*    @GET("api/v1/records/{id}")
+    Call<> getAlbumById(@Path("id") int id);
+
+    @POST("api/v1/records")
+    Call<Album> addAlbum(@Body Album album);*/
+}
