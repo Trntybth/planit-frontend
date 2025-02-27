16f24b4b8c4550dc3af8068da4fc3a4f2861fed6
Trinity Booth <trinitybooth89@gmail.com>
2025-02-27 21:09:10 +0000

    feat: added RetroFitInstance class

--- app/src/main/java/com/example/planit_frontend/ApiService.java
+++ app/src/main/java/com/example/planit_frontend/ApiService.java
@@ -5,9 +5,5 @@ import retrofit2.http.GET;
 import retrofit2.http.POST;
 import retrofit2.http.Path;
 public interface ApiService {
-/*    @GET("api/v1/records/{id}")
-    Call<> getAlbumById(@Path("id") int id);
 
-    @POST("api/v1/records")
-    Call<Album> addAlbum(@Body Album album);*/
 }
---
+++ app/src/main/java/com/example/planit_frontend/RetrofitInstance.java
@@ -0,0 +1,17 @@
+package com.example.planit_frontend;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+public class RetrofitInstance {
+    private static Retrofit retrofit;
+    private static final String BASE_URL = "http://10.0.2.2:8080/";
+
+    public static Retrofit getRetrofitInstance() {
+        if (retrofit == null) {
+            retrofit = new Retrofit.Builder()
+                    .baseUrl(BASE_URL)
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .build();
+        }
+        return retrofit;
+    }
+}
